2ac2f47bbe398ef208213da48568a798b16e6f36
ammyy9908 <sb78639@gmail.com>
2022-10-29 18:13:11 +0530

    Production Endpoint added

--- utils/send_date_message.js
+++ utils/send_date_message.js
@@ -1,30 +1,30 @@
-const axios = require("axios")
-const dotenv = require("dotenv")
-dotenv.config()
+const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
 
 async function sendDateMessage(phone) {
-    const token = process.env.WHATSAPP_TOKEN;
-    try{
-        const r = await axios.post(`https://graph.facebook.com/v14.0/109549668467205/messages`,{
-            "messaging_product": "whatsapp",
-        "to": phone,
-        "type": "template",
-        "template": { "name": "consignment_date", "language": { "code": "en_US" } ,
-    
-    }
-},{
-    headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`
-    }
-})
-console.log(r.data)
-return r.data;
-    }
-
-    catch(e){
-console.log(e);
-    }
+  const token = process.env.WHATSAPP_TOKEN;
+  try {
+    const r = await axios.post(
+      `https://graph.facebook.com/v15.0/107625022002368/messages`,
+      {
+        messaging_product: "whatsapp",
+        to: phone,
+        type: "template",
+        template: { name: "consignment_date", language: { code: "en_US" } },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log(r.data);
+    return r.data;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 module.exports = sendDateMessage;
